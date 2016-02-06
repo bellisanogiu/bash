c3f350c613d7a885a70c27e36a0361cd3e2afd7c
Giuseppe Bellisano <bellisano@gmail.com>
2016-02-06 18:10:16 +0100

    backup

---
+++ copiaHTML.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#echo 'Backup della cartella /var/www/html/'
+#cd /home/pino/Dropbox/
+#tar -czvf tmp/bachtml.tar.gz /var/www/html
+#echo 'Backup terminato'
--- template.sh
+++ template.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-echo ''
+echo 'backup della cartella /var/www/html/'
+tar -zcvf /home/pino/Dropbox/tmp/htm/html-$(date +%F).tar.gz /var/www/html
+echo 'backup terminato'
 
